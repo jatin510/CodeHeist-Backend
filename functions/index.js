db1bb7801478d080bfef271c30f6cf22d4d688a1
Jagdish <jatin6972@gmail.com>
2020-05-26 03:29:05 +0530

    issue fixed : express is working again

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
-// const db = admin.firestore();
+const db = admin.firestore();
 
 const express = require("express");
 const app = express();
@@ -14,23 +14,43 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  console.log("hello");
-  return res.send("hello");
+  console.log("API CALLED");
+  // db.collection("main").add("jatin").set({ name: "jatin" });
+  return res.send("API CALLED");
 });
 
 app.get("/hello", (req, res) => {
-  console.log("hello");
-  res.send("hello");
+  console.log("API HELLO");
+  res.send("API HELLO");
 });
 
-exports.api = functions.https.onRequest(app);
+const api = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
+const helloWorld = functions.https.onRequest((request, response) => {
+  console.log("hello from firebase");
+  let data = {
+    name: "Los Angeles",
+    state: "CA",
+    country: "USA",
+  };
+
+  // Add a new document in collection "cities" with ID 'LA'
+  let setDoc = db
+    .collection("main")
+    .doc("LA")
+    .set(data)
+    .then((data) => console.log("created", data))
+    .catch((err) => console.log("error", err));
   response.send("Hello from Firebase!");
 });
+
+module.exports = {
+  helloWorld,
+  api,
+};
